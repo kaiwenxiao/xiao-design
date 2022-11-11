cfd23e729275cc4eb82306abd72ea89ec116f230
xiao kaiwen <xiaokw@dragonpass.com.cn>
2022-11-11 21:45:33 +0800

    feat: button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button, { ButtonSize, ButtonType } from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button autoFocus>Hello</Button>
+      <Button
+        onClick={(e) => {
+          alert("haha");
+        }}
+        btnType={ButtonType.Primary}
+        size={ButtonSize.Large}
+      >
+        {" "}
+        hello
+      </Button>
+      <Button
+        className="custom"
+        btnType={ButtonType.Link}
+        size={ButtonSize.Large}
+        href="www.baidu.com"
+      >
+        {" "}
+        Link
+      </Button>
+      <Button btnType={ButtonType.Default} size={ButtonSize.Large} disabled>
+        {" "}
+        Default button
+      </Button>
+      <Button
+        btnType={ButtonType.Link}
+        size={ButtonSize.Large}
+        href="www.baidu.com"
+        disabled
+      >
+        {" "}
+        Disable Link button
+      </Button>
+      <Button
+        btnType={ButtonType.Link}
+        size={ButtonSize.Large}
+        href="www.baidu.com"
+      >
+        {" "}
+        Link button
+      </Button>
     </div>
   );
 }
